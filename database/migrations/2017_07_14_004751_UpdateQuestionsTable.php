06507397c6e4f06f36c0bfff3af59de4bd153061
mrashidcit <mrashidcit@gmail.com>
2017-07-14 06:55:37 +0500

    Questions table is Updated..

---
+++ database/migrations/2017_07_14_004751_UpdateQuestionsTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function(Blueprint $table){
+            $table->integer('board_id')->unsigned();
+            $table->integer('year_id')->unsigned();
+            $table->integer('class_id')->unsigned();
+            //$table->integer('subject_id')->unsigned();
+            
+        });
+        
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
